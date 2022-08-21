145b4bad92f920a8f498e6a0491fa7492337123a
Dawid Pater <kaokes@gmail.com>
2022-08-21 17:30:04 +0200

    Added language selector

--- src/main/java/com/dawidpater/project/carrental/config/AppConfig.java
+++ src/main/java/com/dawidpater/project/carrental/config/AppConfig.java
@@ -3,15 +3,22 @@ package com.dawidpater.project.carrental.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Locale;
 
 @Configuration
 //@EnableWebMvc
 //@EnableSwagger2
 @EnableTransactionManagement
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket swaggerApi() {
@@ -19,6 +26,25 @@ public class AppConfig {
                 .select()
                 .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
                 .build();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
 
